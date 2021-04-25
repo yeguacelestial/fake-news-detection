af3fb724dc728a0bc8c2f8c00ce99b4aaa771eda
Yegua Celestial <yeguacelestial@gmail.com>
2021-04-25 18:49:01 -0500

    Update templates

--- fake_news_detection/urls.py
+++ fake_news_detection/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
 from fake_news_detection import views
 
-app_name = 'fake_news_detection'
 
 urlpatterns = [
     path('', views.index, name="home"),
     path('result/', views.result, name="result"),
     path('satisfaction/', views.satisfaction, name="satisfaction"),
-    path('test/', views.base, name="base")
 ]
