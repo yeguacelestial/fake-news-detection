a8d4d475b17dce97b9e5de808fe0e48aac426931
Yegua Celestial <yeguacelestial@gmail.com>
2021-04-25 19:23:42 -0500

    Add heroku files

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fakenews-platform.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
